774aa1c33c762d41fc4c8959f9a7f8fd1b486333
Dennis.Markmann <Dennis.Markmann@Dennis.local>
2016-07-01 01:36:48 +0200

    Added Geldbetrag Typ and Test. Removed unused TODOs.

---
+++ Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/Geldbetrag.java
@@ -0,0 +1,104 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.barzahlung;
+
+public class Geldbetrag
+{
+    private int _euroBetrag = 0;
+    private int _centBetrag = 0;
+
+    Geldbetrag(String wert)
+    {
+        if (!validate(wert))
+        {
+            return;
+            //TODO ungültig
+        }
+        if (wert.contains(","))
+        {
+            _euroBetrag = Integer.parseInt(wert.substring(0, wert.indexOf(",")));
+            _centBetrag = Integer.parseInt(wert.substring(wert.indexOf(",") + 1));
+        }
+        else
+        {
+            _centBetrag = Integer.parseInt(wert);
+        }
+    }
+
+    Geldbetrag(int wert)
+    {
+        _centBetrag = wert;
+    }
+
+    Geldbetrag addiere(Geldbetrag additionsBetrag)
+    {
+        _euroBetrag += additionsBetrag.getEuroBetrag();
+        _centBetrag += additionsBetrag.getCentBetrag();
+        haendleCentUeberschuss();
+        return this;
+    }
+
+    Geldbetrag subtrahiere(Geldbetrag subtraktionsBetrag)
+    {
+        _euroBetrag -= subtraktionsBetrag.getEuroBetrag();
+        _centBetrag -= subtraktionsBetrag.getCentBetrag();
+        haendleCentUeberschuss();
+        return this;
+    }
+
+    Geldbetrag multipliziere(int multiplikator)
+    {
+        _euroBetrag *= multiplikator;
+        _centBetrag *= multiplikator;
+        haendleCentUeberschuss();
+        return this;
+    }
+
+    private void haendleCentUeberschuss()
+    {
+        int euroAnpassung = 0;
+        while (_centBetrag < 0)
+        {
+            euroAnpassung--;
+            _centBetrag += 100;
+        }
+        while (_centBetrag > 100)
+        {
+            euroAnpassung++;
+            _centBetrag -= 100;
+        }
+        _euroBetrag += euroAnpassung;
+    }
+
+    private boolean validate(String wert)
+    {
+        //TODO check more stupid stuff
+        //Symbols / Centvalue > 100 / Centvalue only 1 long
+        int counter = 0;
+        for (char c : wert.toCharArray())
+        {
+            if (c == ',')
+            {
+                counter++;
+                if (counter == 2)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public int getEuroBetrag()
+    {
+        return _euroBetrag;
+    }
+
+    public int getCentBetrag()
+    {
+        return _centBetrag;
+    }
+
+    String gibGeldbetragDarstellung()
+    {
+        return _euroBetrag + "," + _centBetrag;
+    }
+}
---
+++ Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/GeldbetragTest.java
@@ -0,0 +1,107 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.barzahlung;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GeldbetragTest
+{
+    private String _wertEins;
+    private int _wertEinsEuro;
+    private int _wertEinsCent;
+    private String _wertZwei;
+    private int _wertZweiEuro;
+    private int _wertZweiCent;
+    private int _multiplikator;
+
+    @Before
+    public void createTestData()
+    {
+        _wertEinsEuro = 59;
+        _wertEinsCent = 36;
+        _wertEins = _wertEinsEuro + "," + _wertEinsCent;
+
+        _wertZweiEuro = 123;
+        _wertZweiCent = 96;
+        _wertZwei = _wertZweiEuro + "," + _wertZweiCent;
+
+        _multiplikator = 5;
+    }
+
+    @Test
+    public void testeGeldbetragKonstruktoren()
+    {
+        Geldbetrag betragEins = new Geldbetrag(_wertEins);
+        Geldbetrag betragZwei = new Geldbetrag(_wertZweiCent);
+
+        assertEquals(_wertEinsEuro, betragEins.getEuroBetrag());
+        assertEquals(_wertEinsCent, betragEins.getCentBetrag());
+        assertEquals(_wertZweiCent, betragZwei.getCentBetrag());
+    }
+
+    @Test
+    public void testeAddiereGeldbetraege()
+    {
+        Geldbetrag grundBetrag = new Geldbetrag(_wertEins);
+        Geldbetrag zuAddierenderBetrag = new Geldbetrag(_wertZwei);
+        grundBetrag = grundBetrag.addiere(zuAddierenderBetrag);
+
+        int centGesamt = _wertEinsCent + _wertZweiCent;
+        int euroGesamt = _wertEinsEuro + _wertZweiEuro;
+        int zuAddierendeEuro = centGesamt / 100;
+        centGesamt -= zuAddierendeEuro * 100;
+        euroGesamt += zuAddierendeEuro;
+
+        assertEquals(euroGesamt, grundBetrag.getEuroBetrag());
+        assertEquals(centGesamt, grundBetrag.getCentBetrag());
+    }
+
+    @Test
+    public void testeSubtrahiereGeldbetraege()
+    {
+        Geldbetrag grundBetrag = new Geldbetrag(_wertEins);
+        Geldbetrag zuSubtrahierenderBetrag = new Geldbetrag(_wertZwei);
+        grundBetrag = grundBetrag.subtrahiere(zuSubtrahierenderBetrag);
+
+        int centGesamt = _wertEinsCent - _wertZweiCent;
+        int euroGesamt = _wertEinsEuro - _wertZweiEuro;
+
+        //TODO nicht 1:1 wie im Code
+        int zuSubtrahierendeEuro = 0;
+        while (centGesamt < 0)
+        {
+            zuSubtrahierendeEuro++;
+            centGesamt += 100;
+        }
+        euroGesamt -= zuSubtrahierendeEuro;
+
+        assertEquals(euroGesamt, grundBetrag.getEuroBetrag());
+        assertEquals(centGesamt, grundBetrag.getCentBetrag());
+    }
+
+    @Test
+    public void testeMultipliziere()
+    {
+        Geldbetrag geldbetrag = new Geldbetrag(_wertEins);
+        geldbetrag = geldbetrag.multipliziere(_multiplikator);
+
+        int expectedEuroBetrag = _wertEinsEuro * _multiplikator;
+        int expectedCentBetrag = _wertEinsCent * _multiplikator;
+
+        int zuAddierendeEuro = expectedCentBetrag / 100;
+        expectedCentBetrag -= zuAddierendeEuro * 100;
+        expectedEuroBetrag += zuAddierendeEuro;
+
+        assertEquals(expectedEuroBetrag, geldbetrag.getEuroBetrag());
+        assertEquals(expectedCentBetrag, geldbetrag.getCentBetrag());
+    }
+
+    @Test
+    public void testeGibGeldbetragDarstellung()
+    {
+        Geldbetrag geldbetrag = new Geldbetrag(_wertEins);
+        String stringDarstellung = geldbetrag.gibGeldbetragDarstellung();
+        assertEquals(_wertEinsEuro + "," + _wertEinsCent, stringDarstellung);
+    }
+}
